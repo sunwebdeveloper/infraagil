aad84d22fbfde147ad42a288378584276784bc5b
Rafael <sunwebdeveloper@gmail.com>
2020-03-10 22:33:28 -0300

    Aula de configuracao do Jenkins

---
+++ src/main/java/br/com/alura/forum/config/DatasourceConfiguration.java
@@ -0,0 +1,13 @@
+package br.com.alura.forum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+public class DatasourceConfiguration {
+
+
+}
